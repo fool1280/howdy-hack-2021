a17f35639e82c152c34ed7ddb73339d59f7470de
Anh Nguyen <ng.huyanh.gd@gmail.com>
2021-09-11 18:30:51 -0500

    add + post request

---
+++ backend/server_fer/ferapp/views.py
@@ -0,0 +1,36 @@
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
+import cv2
+import os
+from fer import FER
+# Create your views here.
+
+
+class PostView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+
+class PostCreate(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        posts_serializer = PostSerializer(data=request.data)
+        if posts_serializer.is_valid():
+            posts_serializer.save()
+            image = cv2.imread(os.getcwd() + posts_serializer.data['image'])
+            detector = FER(mtcnn=True)
+            result = detector.top_emotion(image)
+            print(result)
+            return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print('error', posts_serializer.errors)
+            return Response(posts_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
