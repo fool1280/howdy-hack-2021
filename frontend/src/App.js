057ffdc3e093a272ebaa3cfac62fc9911f08aebc
Prometheus1400 <prometheus1400@gmail.com>
2021-09-11 13:06:36 -0500

    added button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "@material-ui/core/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                margin: "auto",
+                width: 400,
+                flexWrap: "wrap",
+                justifyContent: "center",
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            <Button variant="contained" color="Secondary">
+                Secondary Button
+            </Button>
+        </div>
+    );
+};
 
 export default App;
