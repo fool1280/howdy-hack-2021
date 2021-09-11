b6f4e1a02219286c130d0fdecfca8bb8a424bc00
phuhgnguyen <phu.h.g.nguyen@gmail.com>
2021-09-11 13:43:29 -0500

    Styled Button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,33 @@
 import React, { useEffect, useRef } from "react";
-
+import Button from "@material-ui/core/Button";
 const App = () => {
-  const videoRef = useRef(null);
+    const videoRef = useRef(null);
 
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
+    useEffect(() => {
+        getVideo();
+    }, [videoRef]);
 
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { width: 300 } })
-      .then(stream => {
-        let video = videoRef.current;
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(err => {
-        console.error("error:", err);
-      });
-  };
+    const getVideo = () => {
+        navigator.mediaDevices
+            .getUserMedia({ video: { width: 300 } })
+            .then(stream => {
+                let video = videoRef.current;
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(err => {
+                console.error("error:", err);
+            });
+    };
 
-  return (
-    <div>
-      <div>
-        <button>Take a photo</button>
-        <video ref={videoRef} />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <Button variant="contained" color="secondary">Take a photo</Button>
+                <video ref={videoRef} />
+            </div>
+        </div>
+    );
 };
 
 export default App;
