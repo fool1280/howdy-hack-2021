2f0d00ea92c0166b2c68261095e4c60666a48355
hnathehe2 <hnathehe2@DESKTOP-B33SRFL.localdomain>
2021-09-11 19:05:25 -0500

    update get names

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           Learn React
         </a>
 
-        < GetRequest/>
+        < GetRequest />
       </header>
     </div>
   );
--- src/components/GetRequest.js
+++ src/components/GetRequest.js
@@ -5,24 +5,46 @@ const access_token ='BQD417-aQtsPjQ17yzaom4D9GhBwjXrUMKuDbCV4Ha0pOann5yBKG1JdA05
 
 function GetRequest() {
     const [suggestSong, setSuggestSong] = useState([]);
+    const [market, setMarket] = useState('US')
+    const [gernes, setGernes] = useState('')
+    const [minDanceability, setMinDanceability] = useState(0);
+    const [maxDanceability, setMaxDanceability] = useState(0);
+    const [minEnergy, setMinEnergy] = useState(0);
+    const [maxEnergy, setMaxEnergy] = useState(0);
+    const [minLoudness, setMinLoudness] = useState(0);
+    const [maxLoudness, setMaxLoudness] = useState(0);
+    const [minPopularity, setMinPopularity] = useState(0);
+    const [maxPopularity, setMaxPopularity] = useState(0);
+    const [minValence, setMinValence] = useState(0);
+    const [maxValence, setMaxValence] = useState(0);
 
+    // const getMood = async() => {
+    //     let url = "placeholder" //fix later
+
+    // } 
+
+    const getInfoPlaceHolder = async() => {
+        setMarket('US');
+        setGernes('country');
+        getSuggestSong();
+    }
 
     const getSuggestSong = async() => {
-        let url = 'https://api.spotify.com/v1/recommendations?limit=3&market=ES&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA';
+        console.log(market);
+        let url = 'https://api.spotify.com/v1/recommendations?limit=3&market='+market+'&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres='+gernes+'&seed_tracks=0c6xIDDpzE81m2q797ordA';
         let result = await axios.get(url, {headers: {
                 'Authorization': `Bearer ${access_token}`,
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
                 }
         }).then((res) => res.data.tracks);
-        console.log("result: ", result);
         setSuggestSong(result);
         console.log("suggestSong: ", suggestSong);
     }
     useEffect(() => {
-        getSuggestSong();
+        getInfoPlaceHolder(); //fix to get info later
     }, [])
-    if (suggestSong.length == 0) {
+    if (suggestSong.length === 0) {
         return (
             <a>
                 Loading .........
@@ -30,8 +52,8 @@ function GetRequest() {
         )
     } else return (
         <div>
-            temp
             {suggestSong.map((item) => <p>{item.name}</p>)}
+            <p>{market}</p>
         </div>
     )
 }
