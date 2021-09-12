323d7d00718110c08fddfc7927af2a71a9d0689c
Anh Nguyen <ng.huyanh.gd@gmail.com>
2021-09-12 10:27:53 -0500

    merge get spotify song to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,39 @@ import axios from "axios";
 import "./App.css";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import SpotifyPlayer from "react-spotify-player";
+import GetSongRequest from "./components/GetSongRequest";
 // import SpotifyPlayer from "react-spotify-web-playback";
+
+const access_token =
+    "BQBbE9FdjKQ97KLfbsVcKmBmlapBMAw_JhYSme7trxROwwuVqhZMDZpcxckbd0XhqBtyUdFjxbL1RznHgIeuFdxG5Xz172YidddD6j7U9KvkJigvPWbeWz4kgi3WXidMKfTagTksiTX-_5_EfaSDZcHd3WPaN_19SwbAt0fBCUs";
 
 const App = () => {
     const [buttonStatus, setButton] = useState(false);
     const [mood, setMood] = useState("");
     const webcamRef = React.useRef(null);
     const [imgSrc, setimgSrc] = useState(null);
+    const allMood = [
+        "sad",
+        "angry",
+        "fear",
+        "happy",
+        "disgust",
+        "neutral",
+        "surprise",
+    ];
+    const allGerne = [];
+    const allMinDanceability = [0.35, 0.63, 0.2, 0.64, 0.21, 0.29, 1];
+    const allMaxDanceability = [0.5, 0.85, 0.45, 0.8, 0.4, 0.4, 1];
+    const allMinEnergy = [0.2, 0.5, 0.0, 0.65, 0.7, 0.0, 1];
+    const allMaxEnergy = [0.6, 1, 0.2, 1.0, 0.9, 0.6, 1];
+    const allMinLoudness = [-10.0, -10, -40.0, -10.0, -10, -25, 1];
+    const allMaxLoudness = [0.0, 0.0, -15.0, 0.0, 0.0, -5, 1];
+    //const allMinPopularity =    [ 0.5 ]
+    //const allMaxPopularity =    [ 1]
+    const allMinValence = [0.1, 0.25, 0.0, 0.7, 0.1, 0.0, 1];
+    const allMaxValence = [0.5, 0.5, 0.5, 1.0, 0.4, 0.4, 1];
+    const allMinTempo = [50, 80, 90, 90, 80, 60, 1];
+    const allMaxTempo = [200, 150, 180, 180, 170, 200, 1];
 
     const capture = React.useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
@@ -34,13 +60,15 @@ const App = () => {
             .then((result) => result.data);
         setMood(result);
         setButton(false);
-        console.log("mood:", mood);
     };
     useEffect(() => {
         if (!(imgSrc === null)) {
             sendData(imgSrc);
         }
-    }, [imgSrc]);
+        if (!(mood === "")) {
+            console.log("mood:", mood);
+        }
+    }, [imgSrc, mood]);
     return (
         <div style={{ alignItems: "center", textAlign: "center" }}>
             <h1>mello.</h1>
@@ -127,12 +155,7 @@ const App = () => {
                         />
                     )}
                 </div>
-
-                {/* <SpotifyPlayer
-                token="BQA4O_zFd-knN-WC76MKILhomAS3rO6koJ0CVLI2kyt9z0LLTDtgR85gItqi8rFYGF4LJS1W88lOmWs3uSDOa-bSrjSR-a1O0vn49BqXKdXbAKxeBhqmGNk34ct0sr4CUwIBDS_cNKQPP2cfisti6_svsuBUu1E"
-                uris={["spotify:artist:6HQYnRM4OzToCYPpVBInuU"]}
-            /> */}
-                <div
+                {/* <div
                     style={{
                         width: "50%",
                         justifyContent: "center",
@@ -145,6 +168,22 @@ const App = () => {
                         view="coverart"
                         theme="black"
                     />
+                </div> */}
+                <div
+                    style={{
+                        width: "50%",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <div>
+                        <SpotifyPlayer
+                            uri="spotify:track:4cOdK2wGLETKBW3PvgPWqT"
+                            width="100%"
+                            view="coverart"
+                            theme="black"
+                        />
+                    </div>
                 </div>
                 <a href="https://github.com/fool1280/howdy-hack-2021">
                     <GitHubIcon
@@ -158,6 +197,7 @@ const App = () => {
                     />{" "}
                 </a>
             </div>
+            <GetSongRequest />
         </div>
     );
 };
